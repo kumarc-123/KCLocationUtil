6a37ee3de70b08f3a934798f79906fd2cd844979
Kumar C <kumarchikkanna1@gmail.com>
2016-04-19 12:51:43 +0530

    Initial Commit

---
+++ KCLocationUtil/KCLocationUtil.h
@@ -0,0 +1,19 @@
+//
+//  KCLocationUtil.h
+//  KCLocationUtil
+//
+//  Created by Kumar C on 4/19/16.
+//  Copyright © 2016 Kumar C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KCLocationUtil.
+FOUNDATION_EXPORT double KCLocationUtilVersionNumber;
+
+//! Project version string for KCLocationUtil.
+FOUNDATION_EXPORT const unsigned char KCLocationUtilVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KCLocationUtil/PublicHeader.h>
+
+
