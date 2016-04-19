2b121195e6e00ae19d83d76041cd928620bf1ab7
Kumar C <kumarchikkanna1@gmail.com>
2016-04-19 15:13:49 +0530

    Added KCLocationManager reference

--- KCLocationUtil/KCLocationUtil.h
+++ KCLocationUtil/KCLocationUtil.h
@@ -17,3 +17,4 @@ FOUNDATION_EXPORT const unsigned char KCLocationUtilVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <KCLocationUtil/PublicHeader.h>
 
 
+#import "KCLocationManager.h"
